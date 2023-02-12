ff26cd7e6e9ff76273960fb6e5e9c1e808bf9b48
lestece <loiceleste94@gmail.com>
2023-02-12 16:48:50 +0000

    Add some tests and build the function

--- evens.py
+++ evens.py
@@ -3,11 +3,27 @@ def even_number_of_evens(numbers):
     Should raise a TypeError if a list is not passed into the function
     error message: "A list was not passed into the function"
     if numbers is empty return False
-    if the number of even numbers is odd, terurn False
-    if the number of even numvers is 0, return False
+    if the number of even numbers is odd, return False
+    if the number of even numbers is 0, return False
     if the number of even numbers is even, return True
     """
-    return None
+    if isinstance(numbers, list):
+        if numbers == []:
+            return False
+        else:
+            evens = 0
+
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
+
+    else:
+        raise TypeError("A list was not passed into the function")
 
 
 if __name__ == '__main__':  
